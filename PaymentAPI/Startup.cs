1bcabb46dfd1b6883e1c513179643b5d9ca2a3b0
Teenapat <teenapat.wu@gmail.com>
2021-06-26 23:41:15 +0700

    🐛 Bugfix call MapControllers

--- PaymentAPI/Startup.cs
+++ PaymentAPI/Startup.cs
@@ -50,6 +50,7 @@ namespace PaymentAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
